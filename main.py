dfb5a7df45c82fbcf09234902be9a20aa7c26a0c
jeter1011 <erickagudelo123@gmail.com>
2021-04-18 18:26:35 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# if __name__ == '__main__':
+# TODO: Fix BUG
+from person import Person
+
+
+
+def hello(name: str):
+    print(name.upper())
+
+
+def hello2(name: str):
+    print(name.upper())
+
+
+def alexMethod(name):
+    P = Person(name)
+    print(P)
+
+
+if __name__ == '__main__':
+
+    erick = "Erick"
+    print(erick)
+    alexMethod("ali")
